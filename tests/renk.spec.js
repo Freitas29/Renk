fd02a6ce7b4be09592c0cc5bd9d8203335d8b0d2
Arão Freitas <arao@f360.com.br>
2020-07-28 16:28:39 -0300

    test to rename and rename only keys

--- tests/renk.spec.js
+++ tests/renk.spec.js
@@ -1,7 +1,49 @@
 import Renk from '../src/index'
 
-test('adds 1 + 2 to equal 3', () => {
+const initialObject  = {
+    name: "Bob",
+    lastName: "Foo",
+    age: 16,
+    gender: "male",
+    f: [{
+        name: "rice"
+    },{
+        name: "bean"
+    }, {
+        name: "steak"
+    }]
+}
+
+test('Should rename key f and return rest', () => {
+    const anotherObject = new Renk(initialObject)
+    const newObject = anotherObject.rename({f: "favoriteFoods"})
     
+    expect(newObject).toEqual(
+        expect.objectContaining({
+        favoriteFoods: expect.any(Array),
+        name: expect.any(String),
+    }))
+});
+
+test('Should rename key f and only favoriteFoods', () => {
+    const anotherObject = new Renk(initialObject)
+    const newObject = anotherObject.renameOnly({f: "favoriteFoods"})
     
-    expect(1 + 2).toBe(3);
+    expect(newObject).toEqual(
+        expect.objectContaining({
+        favoriteFoods: expect.any(Array),
+    }))
+});
+
+test('Should rename only key f and not return rest keys', () => {
+    const anotherObject = new Renk(initialObject)
+    const newObject = anotherObject.renameOnly({f: "favoriteFoods"})
+    
+    expect(newObject).toEqual(
+        expect.not.objectContaining({
+        name:  expect.any(String),
+        lastName: expect.any(String),
+        age: expect.any(Number),
+        gender: expect.any(String),
+    }))
 });
