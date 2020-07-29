535f089912072e34a8e94a40f097a0dbba2b3dfa
Arão Freitas <arao@f360.com.br>
2020-07-29 10:26:16 -0300

    Test to change value and key

--- tests/renk.spec.js
+++ tests/renk.spec.js
@@ -1,7 +1,7 @@
 import Renk from '../src/index'
 
 const initialObject  = {
-    name: "Bob",
+    firstName: "Bob",
     lastName: "Foo",
     age: 16,
     gender: "male",
@@ -21,7 +21,7 @@ test('Should rename key f and return rest', () => {
     expect(newObject).toEqual(
         expect.objectContaining({
         favoriteFoods: expect.any(Array),
-        name: expect.any(String),
+        firstName: expect.any(String),
     }))
 });
 
@@ -41,9 +41,48 @@ test('Should rename only key f and not return rest keys', () => {
     
     expect(newObject).toEqual(
         expect.not.objectContaining({
-        name:  expect.any(String),
+        firstName:  expect.any(String),
         lastName: expect.any(String),
         age: expect.any(Number),
         gender: expect.any(String),
     }))
+
+    expect(newObject).toEqual(
+        expect.objectContaining({
+        favoriteFoods: expect.any(Array),
+    }))
 });
+
+test("Should change key and value and return rest keys", () => {
+    const anotherObject = new Renk(initialObject)
+    const newObject = anotherObject.rename({
+        firstName: ["fullName", person => `${person.name} ${person.lastName}`]
+    })
+
+    expect(newObject).toEqual(
+        expect.objectContaining({
+        fullName: `${initialObject.name} ${initialObject.lastName}`,
+        lastName: expect.any(String),
+        age: expect.any(Number),
+        gender: expect.any(String),
+    }))
+})
+
+test("Should change key and value and not return rest keys", () => {
+    const anotherObject = new Renk(initialObject)
+    const newObject = anotherObject.renameOnly({
+        firstName: ["fullName", person => `${person.name} ${person.lastName}`]
+    })
+
+    expect(newObject).toEqual(
+        expect.objectContaining({
+        fullName: `${initialObject.name} ${initialObject.lastName}`,
+    }))
+
+    expect(newObject).toEqual(
+        expect.not.objectContaining({
+        lastName: expect.any(String),
+        age: expect.any(Number),
+        gender: expect.any(String),    
+    }))
+})
